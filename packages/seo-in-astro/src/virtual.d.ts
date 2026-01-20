5668cbd8752a7c124384a9d921712b53bcaa6187
Daniel Castillo <dlcastillo3015@gmail.com>
2026-01-20 07:51:07 -0500

    feat(seo-in-astro): add custom sitemap generation

--- packages/seo-in-astro/src/virtual.d.ts
+++ packages/seo-in-astro/src/virtual.d.ts
@@ -6,6 +6,22 @@ declare module "virtual:seo-in-astro/config" {
     siteName: string;
     defaultOgImg: string;
     manualRoutes: string[];
+    sitemapConfig?:
+      | {
+          route: string;
+          lastModified?: string | Date | undefined;
+          changeFrequency?:
+            | "never"
+            | "always"
+            | "hourly"
+            | "daily"
+            | "weekly"
+            | "monthly"
+            | "yearly"
+            | undefined;
+          priority?: number | undefined;
+        }[]
+      | undefined;
   };
 
   const config: SeoInAstroConfig;
