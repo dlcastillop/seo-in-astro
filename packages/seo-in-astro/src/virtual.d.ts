dfd3cef04d89b06bae3d2caf46bea8bba93ed615
Daniel Castillo <dlcastillo3015@gmail.com>
2026-01-21 08:58:07 -0500

    chore(seo-in-astro): update virtual

--- packages/seo-in-astro/src/virtual.d.ts
+++ packages/seo-in-astro/src/virtual.d.ts
@@ -5,34 +5,6 @@ declare module "virtual:seo-in-astro/config" {
     baseUrl: string;
     siteName: string;
     defaultOgImg: string;
-    manualRoutes: string[];
-    sitemapConfig?:
-      | {
-          sitemap?:
-            | {
-                route: string;
-                lastModified?: string | Date | undefined;
-                changeFrequency?:
-                  | "never"
-                  | "always"
-                  | "hourly"
-                  | "daily"
-                  | "weekly"
-                  | "monthly"
-                  | "yearly"
-                  | undefined;
-                priority?: number | undefined;
-              }[]
-            | undefined;
-          i18n?:
-            | {
-                defaultLocale: string;
-                locales: Record<string, string>;
-              }
-            | undefined;
-        }
-      | undefined;
-    llmsTxt?: boolean | undefined;
   };
 
   const config: SeoInAstroConfig;
