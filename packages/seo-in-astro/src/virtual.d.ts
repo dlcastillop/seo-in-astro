b4bb02772f420938374a8e340bb3f64595514137
Daniel Castillo <dlcastillo3015@gmail.com>
2026-01-27 06:58:56 -0500

    feat(seo-in-astro): add base LayoutForArticle

--- packages/seo-in-astro/src/virtual.d.ts
+++ packages/seo-in-astro/src/virtual.d.ts
@@ -1,12 +1,12 @@
 // env.d.ts
 
 declare module "virtual:config" {
-  type SeoInAstroConfig = {
-    baseUrl: string;
-    siteName: string;
-    defaultOgImg: string;
-  };
+    type SeoInAstroConfig = {
+        baseUrl: string;
+        siteName: string;
+        defaultOgImg: string;
+    };
 
-  const config: SeoInAstroConfig;
-  export default config;
+    const config: SeoInAstroConfig;
+    export default config;
 }
